cebcf7292a5ffd8a240d9549bd0c8b563ca194eb
spellblade <sohamray24@outlook.com>
2019-01-12 23:17:01 +0530

    Commit 2

--- script.js
+++ script.js
@@ -1,31 +1,36 @@
-
-function start()
-{
-    var hh=parseInt(document.getElementById('hr').value);
-    var mm=parseInt(document.getElementById('min').value);
-    var ss=parseInt(document.getElementById('sec').value);
-    /*for (var i=1;i<=24;i++)
+function start() {
+    var hh = parseInt(document.getElementById('hr').value);
+    var mm = parseInt(document.getElementById('min').value);
+    var ss = parseInt(document.getElementById('sec').value);
+    for (var i=1;i<=24;i++)
     {
+        mm=0;
         for(var j=1;j<=60;j++)
         {
+            ss=0;
             for(var k=1;k<=60;k++)
             {
-                while(k!=60){
-                    ss+=1;
-                    document.getElementById('sec').value=ss;
-                }
+                ss+=1;
+                document.getElementById('sec').value=ss;
             }
-            while(j!=60)
+            if(ss=60)
             {
+                ss=0;
+                document.getElementById('sec').value=ss;
                 mm+=1;
                 document.getElementById('min').value=mm;
             }
         }
-        while(i!=24)
+        if(mm=60)
         {
+            mm=0;
+            document.getElementById('min').value=mm;
             hh+=1;
             document.getElementById('hr').value=hh;
         }
-    }*/
-    
+            
+
+        
+    }
+
 }
