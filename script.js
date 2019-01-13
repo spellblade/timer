9184b139a58fba33c3ed3941ef8e9111ac5f2669
spellblade <sohamray24@outlook.com>
2019-01-13 14:15:32 +0530

    Commit 4.0

--- script.js
+++ script.js
@@ -1,40 +1,89 @@
-function timer()
-{
-    var tm=window.setInterval('start()',1000);
-}
-function start() {
-    var hh = parseInt(document.getElementById('hr').value);
-    var mm = parseInt(document.getElementById('min').value);
-    var ss = parseInt(document.getElementById('sec').value);
-    for (var i=1;i<=24;i++)
-    {
-        mm=0;
-        for(var j=1;j<=60;j++)
-        {
-            ss=0;
-            for(var k=1;k<=60;k++)
-            {
-                ss+=1;
-                document.getElementById('sec').value=ss;
-            }
-            if(ss=60)
-            {
-                ss=0;
-                document.getElementById('sec').value=ss;
-                mm+=1;
-                document.getElementById('min').value=mm;
-            }
-        }
-        if(mm=60)
-        {
-            mm=0;
-            document.getElementById('min').value=mm;
-            hh+=1;
-            document.getElementById('hr').value=hh;
-        }
-            
+window.onload = function () {
 
-        
+    var seconds = 00;
+    var tens = 00;
+    var mins=00;
+    var hrs=00;
+    var appendTens = document.getElementById("tens")
+    var appendSeconds = document.getElementById("seconds")
+    var appendMins=document.getElementById("mins");
+    var appendHrs=document.getElementById("hrs");
+    var buttonStart = document.getElementById('button-start');
+    var buttonStop = document.getElementById('button-stop');
+    var buttonReset = document.getElementById('button-reset');
+    var Interval;
+
+    buttonStart.onclick = function () {
+
+        clearInterval(Interval);
+        Interval = setInterval(startTimer, 10);
     }
+
+    buttonStop.onclick = function () {
+        clearInterval(Interval);
+    }
+
+
+    buttonReset.onclick = function () {
+        clearInterval(Interval);
+        tens = "00";
+        seconds = "00";
+        mins="00";
+        hrs="00";
+        appendTens.innerHTML = tens;
+        appendSeconds.innerHTML = seconds;
+        appendMins.innerHTML=mins;
+        appendHrs.innerHTML=hrs;
+    }
+
+
+
+    function startTimer() {
+        tens++;
+
+        if (tens < 9) {
+            appendTens.innerHTML = "0" + tens;
+        }
+
+        if (tens > 9) {
+            appendTens.innerHTML = tens;
+
+        }
+
+        if (tens > 99) {
+            console.log("seconds");
+            seconds++;
+            appendSeconds.innerHTML = "0" + seconds;
+            tens = 0;
+            appendTens.innerHTML = "0" + 0;
+        }
+
+        if (seconds > 9) {
+            appendSeconds.innerHTML = seconds;
+        }
+        if (seconds>59){
+            console.log("minutes");
+            mins++;
+            appendMins.innerHTML="0"+mins;
+            seconds=0;
+            appendSeconds.innerHTML="0"+seconds;
+        }
+        if(mins>9){
+            appendMins.innerHTML=mins;
+        }
+        if(mins>59){
+            console.log("hours");
+            hrs++;
+            appendHrs.innerHTML="0"+hrs;
+            mins=0;
+            appendMins.innerHTML="0"+mins;
+        }
+        if(hrs>9)
+        {
+            appendHrs.innerHTML=hrs;
+        }
+
+    }
+
 
 }
